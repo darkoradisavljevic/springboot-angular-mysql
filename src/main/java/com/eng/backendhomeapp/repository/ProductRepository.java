c82e34aab9ffb71e862b9ab4b608054d8e8b0688
Darko Radisavljevic <darko.radisavljevic@hotmail.com>
2021-07-01 11:25:18 +0200

    modf

--- src/main/java/com/eng/backendhomeapp/repository/ProductRepository.java
+++ src/main/java/com/eng/backendhomeapp/repository/ProductRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.eng.backendhomeapp.entity.Product;
 
 public interface ProductRepository  extends JpaRepository<Product, Integer> {
-
+	final static int number;
 }
